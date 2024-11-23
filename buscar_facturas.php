3476ccc21d3caeddea77c71e87a99916acb9e358
expropiate <118856924+expropiate@users.noreply.github.com>
2024-11-23 01:15:40 -0500

    buscar_facturas.php

--- buscar_facturas.php
+++ buscar_facturas.php
@@ -1,120 +1,111 @@
 <?php
+    /*-------------------------
+    Autor: Obed Alvarado
+    Web: obedalvarado.pw
+    Mail: info@obedalvarado.pw
+    ---------------------------*/
+    include 'is_logged.php'; // Archivo verifica que el usuario que intenta acceder a la URL está logueado
+    /* Connect To Database */
+    require_once "../config/db.php"; // Contiene las variables de configuración para conectar a la base de datos
+    require_once "../config/conexion.php"; // Contiene función que conecta a la base de datos
 
-	/*-------------------------
-	Autor: Obed Alvarado
-	Web: obedalvarado.pw
-	Mail: info@obedalvarado.pw
-	---------------------------*/
-	include('is_logged.php');//Archivo verifica que el usario que intenta acceder a la URL esta logueado
-	/* Connect To Database*/
-	require_once ("../config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
-	require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
-	
-	$action = (isset($_REQUEST['action'])&& $_REQUEST['action'] !=NULL)?$_REQUEST['action']:'';
-	if (isset($_GET['id'])){
-		$numero_factura=intval($_GET['id']);
-		$del1="delete from facturas where numero_factura='".$numero_factura."'";
-		$del2="delete from detalle_factura where numero_factura='".$numero_factura."'";
-		if ($delete1=mysqli_query($con,$del1) and $delete2=mysqli_query($con,$del2)){
-			?>
-			<div class="alert alert-success alert-dismissible" role="alert">
-			  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			  <strong>Aviso!</strong> Datos eliminados exitosamente
-			</div>
-			<?php 
-		}else {
-			?>
-			<div class="alert alert-danger alert-dismissible" role="alert">
-			  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			  <strong>Error!</strong> No se puedo eliminar los datos
-			</div>
-			<?php
-			
-		}
-	}
-	if($action == 'ajax'){
-		// escaping, additionally removing everything that could be (html/javascript-) code
-         $q = mysqli_real_escape_string($con,(strip_tags($_REQUEST['q'], ENT_QUOTES)));
-		  $sTable = "facturas, clientes, users";
-		 $sWhere = "";
-		 $sWhere.=" WHERE facturas.id_cliente=clientes.id_cliente and facturas.id_vendedor=users.user_id";
-		if ( $_GET['q'] != "" )
-		{
-		$sWhere.= " and  (clientes.nombre_cliente like '%$q%' or facturas.numero_factura like '%$q%')";
-			
-		}
-		
-		$sWhere.=" order by facturas.id_factura desc";
-		include 'pagination.php'; //include pagination file
-		//pagination variables
-		$page = (isset($_REQUEST['page']) && !empty($_REQUEST['page']))?$_REQUEST['page']:1;
-		$per_page = 10; //how much records you want to show
-		$adjacents  = 4; //gap between pages after number of adjacents
-		$offset = ($page - 1) * $per_page;
-		//Count the total number of row in your table*/
-		$count_query   = mysqli_query($con, "SELECT count(*) AS numrows FROM $sTable  $sWhere");
-		$row= mysqli_fetch_array($count_query);
-		$numrows = $row['numrows'];
-		$total_pages = ceil($numrows/$per_page);
-		$reload = './facturas.php';
-		//main query to fetch the data
-		$sql="SELECT * FROM  $sTable $sWhere LIMIT $offset,$per_page";
-		$query = mysqli_query($con, $sql);
-		//loop through fetched data
-		if ($numrows>0){
-			echo mysqli_error($con);
-			?>
-			<div class="table-responsive">
-			  <table class="table">
-				<tr  class="info">
-					<th>#</th>
-					<th>Fecha</th>
-					<th>Cliente</th>
-					<th>Vendedor</th>
-					<th>Estado</th>
-					<th class='text-right'>Total</th>
-					<th class='text-right'>Acciones</th>
-					
-				</tr>
-				<?php
-				while ($row=mysqli_fetch_array($query)){
-						$id_factura=$row['id_factura'];
-						$numero_factura=$row['numero_factura'];
-						$fecha=date("d/m/Y", strtotime($row['fecha_factura']));
-						$nombre_cliente=$row['nombre_cliente'];
-						$telefono_cliente=$row['telefono_cliente'];
-						$email_cliente=$row['email_cliente'];
-						$nombre_vendedor=$row['firstname']." ".$row['lastname'];
-						$estado_factura=$row['estado_factura'];
-						if ($estado_factura==1){$text_estado="Pagada";$label_class='label-success';}
-						else{$text_estado="Pendiente";$label_class='label-warning';}
-						$total_venta=$row['total_venta'];
-					?>
-					<tr>
-						<td><?php echo $numero_factura; ?></td>
-						<td><?php echo $fecha; ?></td>
-						<td><a href="#" data-toggle="tooltip" data-placement="top" title="<i class='glyphicon glyphicon-phone'></i> <?php echo $telefono_cliente;?><br><i class='glyphicon glyphicon-envelope'></i>  <?php echo $email_cliente;?>" ><?php echo $nombre_cliente;?></a></td>
-						<td><?php echo $nombre_vendedor; ?></td>
-						<td><span class="label <?php echo $label_class;?>"><?php echo $text_estado; ?></span></td>
-						<td class='text-right'><?php echo number_format ($total_venta,2); ?></td>					
-					<td class="text-right">
-						<a href="editar_factura.php?id_factura=<?php echo $id_factura;?>" class='btn btn-default' title='Editar factura' ><i class="glyphicon glyphicon-edit"></i></a> 
-						<a href="#" class='btn btn-default' title='Descargar factura' onclick="imprimir_factura('<?php echo $id_factura;?>');"><i class="glyphicon glyphicon-download"></i></a> 
-						<a href="#" class='btn btn-default' title='Borrar factura' onclick="eliminar('<?php echo $numero_factura; ?>')"><i class="glyphicon glyphicon-trash"></i> </a>
-					</td>
-						
-					</tr>
-					<?php
-				}
-				?>
-				<tr>
-					<td colspan=7><span class="pull-right"><?php
-					 echo paginate($reload, $page, $total_pages, $adjacents);
-					?></span></td>
-				</tr>
-			  </table>
-			</div>
-			<?php
-		}
-	}
-?>
+    $action = (isset($_REQUEST['action']) && $_REQUEST['action'] != NULL) ? $_REQUEST['action'] : '';
+    if (isset($_GET['id'])) {
+        $id_cliente = intval($_GET['id']);
+        $query = mysqli_query($con, "select * from facturas where id_cliente='" . $id_cliente . "'");
+        $count = mysqli_num_rows($query);
+        if ($count == 0) {
+            if ($delete1 = mysqli_query($con, "DELETE FROM clientes WHERE id_cliente='" . $id_cliente . "'")) {
+                ?>
+                <div class="alert alert-success alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <strong>Aviso!</strong> Datos eliminados exitosamente.
+                </div>
+                <?php
+            } else {
+                ?>
+                <div class="alert alert-danger alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <strong>Error!</strong> Lo siento algo ha salido mal, intenta nuevamente.
+                </div>
+                <?php
+            }
+        } else {
+            ?>
+            <div class="alert alert-danger alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <strong>Error!</strong> No se pudo eliminar este cliente. Existen facturas vinculadas a este producto.
+            </div>
+            <?php
+        }
+    }
+    if ($action == 'ajax') {
+        // escaping, additionally removing everything that could be (html/javascript-) code
+        $q = mysqli_real_escape_string($con, (strip_tags($_REQUEST['q'], ENT_QUOTES)));
+        $aColumns = array('nombre_cliente'); // Columnas de búsqueda
+        $sTable = "clientes";
+        $sWhere = "";
+        if ($_GET['q'] != "") {
+            $sWhere = "WHERE (";
+            for ($i = 0; $i < count($aColumns); $i++) {
+                $sWhere .= $aColumns[$i] . " LIKE '%" . $q . "%' OR ";
+            }
+            $sWhere = substr_replace($sWhere, "", -3);
+            $sWhere .= ')';
+        }
+        $sWhere .= " order by nombre_cliente";
+        include 'pagination.php'; // Include pagination file
+        // pagination variables
+        $page = (isset($_REQUEST['page']) && !empty($_REQUEST['page'])) ? $_REQUEST['page'] : 1;
+        $per_page = 10; // how much records you want to show
+        $adjacents = 4; // gap between pages after number of adjacents
+        $offset = ($page - 1) * $per_page;
+        // Count the total number of rows in your table
+        $count_query = mysqli_query($con, "SELECT count(*) AS numrows FROM $sTable  $sWhere");
+        $row = mysqli_fetch_array($count_query);
+        $numrows = $row['numrows'];
+        $total_pages = ceil($numrows / $per_page);
+        $reload = './clientes.php';
+        // main query to fetch the data
+        $sql = "SELECT * FROM  $sTable $sWhere LIMIT $offset,$per_page";
+        $query = mysqli_query($con, $sql);
+        // loop through fetched data
+        if ($numrows > 0) {
+            ?>
+            <div class="table-responsive">
+                <table class="table">
+                    <tr class="info">
+                        <th>Nombre</th>
+                        <th>Teléfono</th>
+                        <th>Email</th>
+                        <th>Dirección</th>
+                        <th>Estado</th>
+                        <th>Agregado</th>
+                        <th class='text-right'>Acciones</th>
+                    </tr>
+                    <?php
+                    while ($row = mysqli_fetch_array($query)) {
+                        $id_cliente = $row['id_cliente'];
+                        $nombre_cliente = $row['nombre_cliente'];
+                        $telefono_cliente = $row['telefono_cliente'];
+                        $email_cliente = $row['email_cliente'];
+                        $direccion_cliente = $row['direccion_cliente'];
+                        $status_cliente = $row['status_cliente'];
+                        $estado = ($status_cliente == 1) ? "Activo" : "Inactivo";
+                        $date_added = date('d/m/Y', strtotime($row['date_added']));
+                        ?>
+                        <input type="hidden" value="<?php echo $nombre_cliente; ?>" id="nombre_cliente<?php echo $id_cliente; ?>">
+                        <input type="hidden" value="<?php echo $telefono_cliente; ?>" id="telefono_cliente<?php echo $id_cliente; ?>">
+                        <input type="hidden" value="<?php echo $email_cliente; ?>" id="email_cliente<?php echo $id_cliente; ?>">
+                        <input type="hidden" value="<?php echo $direccion_cliente; ?>" id="direccion_cliente<?php echo $id_cliente; ?>">
+                        <input type="hidden" value="<?php echo $status_cliente; ?>" id="status_cliente<?php echo $id_cliente; ?>">
+                        <tr>
+                            <td><?php echo $nombre_cliente; ?></td>
+                            <td><?php echo $telefono_cliente; ?></td>
+                            <td><?php echo $email_cliente; ?></td>
+                            <td><?php echo $direccion_cliente; ?></td>
+                            <td><?php echo $estado; ?></td>
+                            <td><?php echo $date_added; ?></td>
+                            <td><span class="pull-right">
+                                <a href="#" class='btn btn-default' title='Editar cliente' onclick="obtener_datos('<?php echo $id_cliente; ?>');" data-toggle="modal" data-target="#myModal2"><i class="glyphicon glyphicon-edit"></i></a>
+                                <a href="#" class='btn btn-default' title='Borrar cliente' onclick="
