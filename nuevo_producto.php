7cc5e4f3e73df071150d6728ad9e602ffe622483
expropiate <118856924+expropiate@users.noreply.github.com>
2024-11-23 02:06:32 -0500

    nuevo_producto.php

--- nuevo_producto.php
+++ nuevo_producto.php
@@ -1,67 +1,60 @@
 <?php
-include('is_logged.php');//Archivo verifica que el usario que intenta acceder a la URL esta logueado
-	/*Inicia validacion del lado del servidor*/
-	if (empty($_POST['codigo'])) {
-           $errors[] = "Código vacío";
-        } else if (empty($_POST['nombre'])){
-			$errors[] = "Nombre del producto vacío";
-		} else if ($_POST['estado']==""){
-			$errors[] = "Selecciona el estado del producto";
-		} else if (empty($_POST['precio'])){
-			$errors[] = "Precio de venta vacío";
-		} else if (
-			!empty($_POST['codigo']) &&
-			!empty($_POST['nombre']) &&
-			$_POST['estado']!="" &&
-			!empty($_POST['precio'])
-		){
-		/* Connect To Database*/
-		require_once ("../config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
-		require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
-		// escaping, additionally removing everything that could be (html/javascript-) code
-		$codigo=mysqli_real_escape_string($con,(strip_tags($_POST["codigo"],ENT_QUOTES)));
-		$nombre=mysqli_real_escape_string($con,(strip_tags($_POST["nombre"],ENT_QUOTES)));
-		$estado=intval($_POST['estado']);
-		$precio_venta=floatval($_POST['precio']);
-		$date_added=date("Y-m-d H:i:s");
-		$sql="INSERT INTO products (codigo_producto, nombre_producto, status_producto, date_added, precio_producto) VALUES ('$codigo','$nombre','$estado','$date_added','$precio_venta')";
-		$query_new_insert = mysqli_query($con,$sql);
-			if ($query_new_insert){
-				$messages[] = "Producto ha sido ingresado satisfactoriamente.";
-			} else{
-				$errors []= "Lo siento algo ha salido mal intenta nuevamente.".mysqli_error($con);
-			}
-		} else {
-			$errors []= "Error desconocido.";
-		}
-		
-		if (isset($errors)){
-			
-			?>
-			<div class="alert alert-danger" role="alert">
-				<button type="button" class="close" data-dismiss="alert">&times;</button>
-					<strong>Error!</strong> 
-					<?php
-						foreach ($errors as $error) {
-								echo $error;
-							}
-						?>
-			</div>
-			<?php
-			}
-			if (isset($messages)){
-				
-				?>
-				<div class="alert alert-success" role="alert">
-						<button type="button" class="close" data-dismiss="alert">&times;</button>
-						<strong>¡Bien hecho!</strong>
-						<?php
-							foreach ($messages as $message) {
-									echo $message;
-								}
-							?>
-				</div>
-				<?php
-			}
+    include 'is_logged.php'; //Archivo verifica que el usuario que intenta acceder a la URL está logueado
 
+    /* Inicia validación del lado del servidor */
+    if (empty($_POST['nombre'])) {
+        $errors[] = "Nombre vacío";
+    } else if (!empty($_POST['nombre'])) {
+        /* Connect To Database */
+        require_once "../config/db.php"; // Contiene las variables de configuración para conectar a la base de datos
+        require_once "../config/conexion.php"; // Contiene función que conecta a la base de datos
+        
+        // escaping, adicionalmente eliminando todo lo que podría ser código (html/javascript)
+        $nombre = mysqli_real_escape_string($con, (strip_tags($_POST["nombre"], ENT_QUOTES)));
+        $telefono = mysqli_real_escape_string($con, (strip_tags($_POST["telefono"], ENT_QUOTES)));
+        $email = mysqli_real_escape_string($con, (strip_tags($_POST["email"], ENT_QUOTES)));
+        $direccion = mysqli_real_escape_string($con, (strip_tags($_POST["direccion"], ENT_QUOTES)));
+        $estado = intval($_POST['estado']);
+        $date_added = date("Y-m-d H:i:s");
+        
+        $sql = "INSERT INTO clientes (nombre_cliente, telefono_cliente, email_cliente, direccion_cliente, status_cliente, date_added) 
+                VALUES ('$nombre','$telefono','$email','$direccion','$estado','$date_added')";
+        
+        $query_new_insert = mysqli_query($con, $sql);
+        
+        if ($query_new_insert) {
+            $messages[] = "Cliente ha sido ingresado satisfactoriamente.";
+        } else {
+            $errors[] = "Lo siento algo ha salido mal, intenta nuevamente." . mysqli_error($con);
+        }
+    } else {
+        $errors[] = "Error desconocido.";
+    }
+
+    if (isset($errors)) {
+        ?>
+        <div class="alert alert-danger" role="alert">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <strong>Error!</strong>
+            <?php
+            foreach ($errors as $error) {
+                echo $error;
+            }
+            ?>
+        </div>
+        <?php
+    }
+    if (isset($messages)) {
+        ?>
+        <div class="alert alert-success" role="alert">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <strong>¡Bien hecho!</strong>
+            <?php
+            foreach ($messages as $message) {
+                echo $message;
+            }
+            ?>
+        </div>
+        <?php
+    }
 ?>
