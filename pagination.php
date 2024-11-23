775c15ef38641fc5adf601daf86b393916be550c
expropiate <118856924+expropiate@users.noreply.github.com>
2024-11-23 02:13:55 -0500

    pagination.php

--- pagination.php
+++ pagination.php
@@ -1,57 +1,48 @@
 <?php
+// Genera los enlaces para la navegación (anterior, siguiente, etc.)
 function generatePrev($page, $prevlabel) {
     if ($page == 1) {
         return "<li class='disabled'><span><a>$prevlabel</a></span></li>";
-    } else if ($page == 2) {
-        return "<li><span><a href='javascript:void(0);' onclick='load(1)'>$prevlabel</a></span></li>";
-    } else {
-        return "<li><span><a href='javascript:void(0);' onclick='load(" . ($page - 1) . ")'>$prevlabel</a></span></li>";
     }
+    return "<li><span><a href='javascript:void(0);' onclick='load(" . ($page - 1) . ")'>$prevlabel</a></span></li>";
 }
 
+// Genera el enlace para la primera página
 function generateFirstPage($page, $adjacents) {
-    if ($page > ($adjacents + 1)) {
-        return "<li><a href='javascript:void(0);' onclick='load(1)'>1</a></li>";
-    }
-    return '';
+    return ($page > ($adjacents + 1)) ? "<li><a href='javascript:void(0);' onclick='load(1)'>1</a></li>" : '';
 }
 
+// Genera el intervalo de páginas (los puntos suspensivos)
 function generateInterval($page, $adjacents) {
-    if ($page > ($adjacents + 2)) {
-        return "<li><a>...</a></li>";
-    }
-    return '';
+    return ($page > ($adjacents + 2)) ? "<li><a>...</a></li>" : '';
 }
 
+// Genera los enlaces para las páginas intermedias
 function generatePageLinks($page, $adjacents, $tpages) {
-    $pmin = ($page > $adjacents) ? ($page - $adjacents) : 1;
-    $pmax = ($page < ($tpages - $adjacents)) ? ($page + $adjacents) : $tpages;
+    $pmin = max(1, $page - $adjacents);
+    $pmax = min($tpages, $page + $adjacents);
     $links = '';
-    
+
     for ($i = $pmin; $i <= $pmax; $i++) {
-        if ($i == $page) {
-            $links .= "<li class='active'><a>$i</a></li>";
-        } else {
-            $links .= "<li><a href='javascript:void(0);' onclick='load(" . $i . ")'>$i</a></li>";
-        }
+        $links .= ($i == $page) 
+            ? "<li class='active'><a>$i</a></li>"
+            : "<li><a href='javascript:void(0);' onclick='load($i)'>$i</a></li>";
     }
-    
+
     return $links;
 }
 
+// Genera el enlace para la última página
 function generateLastPage($page, $tpages, $adjacents) {
-    if ($page < ($tpages - $adjacents)) {
-        return "<li><a href='javascript:void(0);' onclick='load($tpages)'>$tpages</a></li>";
-    }
-    return '';
+    return ($page < ($tpages - $adjacents)) ? "<li><a href='javascript:void(0);' onclick='load($tpages)'>$tpages</a></li>" : '';
 }
 
+// Genera el enlace para la página siguiente
 function generateNext($page, $tpages, $nextlabel) {
     if ($page < $tpages) {
         return "<li><span><a href='javascript:void(0);' onclick='load(" . ($page + 1) . ")'>$nextlabel</a></span></li>";
-    } else {
-        return "<li class='disabled'><span><a>$nextlabel</a></span></li>";
     }
+    return "<li class='disabled'><span><a>$nextlabel</a></span></li>";
 }
 
 function paginate($reload, $page, $tpages, $adjacents) {
@@ -59,27 +50,13 @@ function paginate($reload, $page, $tpages, $adjacents) {
     $nextlabel = "Next &rsaquo;";
     $out = '<ul class="pagination pagination-large">';
 
-    // Generar el enlace de la página anterior
+    // Concatenar los enlaces generados por las funciones
     $out .= generatePrev($page, $prevlabel);
-
-    // Generar el enlace para la primera página
     $out .= generateFirstPage($page, $adjacents);
-
-    // Generar el intervalo de páginas
     $out .= generateInterval($page, $adjacents);
-
-    // Generar los enlaces de las páginas intermedias
     $out .= generatePageLinks($page, $adjacents, $tpages);
-
-    // Generar un intervalo si es necesario
-    if ($page < ($tpages - $adjacents - 1)) {
-        $out .= "<li><a>...</a></li>";
-    }
-
-    // Generar el enlace para la última página
+    $out .= generateInterval($page, $adjacents);  // Intervalo al final
     $out .= generateLastPage($page, $tpages, $adjacents);
-
-    // Generar el enlace de la página siguiente
     $out .= generateNext($page, $tpages, $nextlabel);
 
     $out .= "</ul>";
