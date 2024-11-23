2d9126808583ec3786cd51e569f831898f852b7e
expropiate <118856924+expropiate@users.noreply.github.com>
2024-11-23 02:19:14 -0500

    productos_factura.php

--- productos_factura.php
+++ productos_factura.php
@@ -5,45 +5,44 @@
 	Web: obedalvarado.pw
 	Mail: info@obedalvarado.pw
 	---------------------------*/
-	include('is_logged.php');//Archivo verifica que el usario que intenta acceder a la URL esta logueado
-	/* Connect To Database*/
-	require_once ("../config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
-	require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
+	include 'is_logged.php'; //Archivo verifica que el usario que intenta acceder a la URL esta logueado
 	
-	$action = (isset($_REQUEST['action'])&& $_REQUEST['action'] !=NULL)?$_REQUEST['action']:'';
-	if($action == 'ajax'){
+	/* Connect To Database*/
+	require_once "../config/db.php"; //Contiene las variables de configuracion para conectar a la base de datos
+	require_once "../config/conexion.php"; //Contiene funcion que conecta a la base de datos
+	
+	$action = (isset($_REQUEST['action']) && $_REQUEST['action'] != NULL) ? $_REQUEST['action'] : '';
+	if ($action == 'ajax') {
 		// escaping, additionally removing everything that could be (html/javascript-) code
-         $q = mysqli_real_escape_string($con,(strip_tags($_REQUEST['q'], ENT_QUOTES)));
-		 $aColumns = array('codigo_producto', 'nombre_producto');//Columnas de busqueda
-		 $sTable = "products";
-		 $sWhere = "";
-		if ( $_GET['q'] != "" )
-		{
+        $q = mysqli_real_escape_string($con, (strip_tags($_REQUEST['q'], ENT_QUOTES)));
+		$aColumns = array('codigo_producto', 'nombre_producto'); //Columnas de busqueda
+		$sTable = "products";
+		$sWhere = "";
+		if ($_GET['q'] != "") {
 			$sWhere = "WHERE (";
-			for ( $i=0 ; $i<count($aColumns) ; $i++ )
-			{
-				$sWhere .= $aColumns[$i]." LIKE '%".$q."%' OR ";
+			for ($i = 0; $i < count($aColumns); $i++) {
+				$sWhere .= $aColumns[$i] . " LIKE '%" . $q . "%' OR ";
 			}
-			$sWhere = substr_replace( $sWhere, "", -3 );
+			$sWhere = substr_replace($sWhere, "", -3);
 			$sWhere .= ')';
 		}
 		include 'pagination.php'; //include pagination file
 		//pagination variables
-		$page = (isset($_REQUEST['page']) && !empty($_REQUEST['page']))?$_REQUEST['page']:1;
+		$page = (isset($_REQUEST['page']) && !empty($_REQUEST['page'])) ? $_REQUEST['page'] : 1;
 		$per_page = 5; //how much records you want to show
 		$adjacents  = 4; //gap between pages after number of adjacents
 		$offset = ($page - 1) * $per_page;
-		//Count the total number of row in your table*/
-		$count_query   = mysqli_query($con, "SELECT count(*) AS numrows FROM $sTable  $sWhere");
-		$row= mysqli_fetch_array($count_query);
+		//Count the total number of row in your table
+		$count_query = mysqli_query($con, "SELECT count(*) AS numrows FROM $sTable  $sWhere");
+		$row = mysqli_fetch_array($count_query);
 		$numrows = $row['numrows'];
-		$total_pages = ceil($numrows/$per_page);
+		$total_pages = ceil($numrows / $per_page);
 		$reload = './index.php';
 		//main query to fetch the data
-		$sql="SELECT * FROM  $sTable $sWhere LIMIT $offset,$per_page";
+		$sql = "SELECT * FROM  $sTable $sWhere LIMIT $offset, $per_page";
 		$query = mysqli_query($con, $sql);
 		//loop through fetched data
-		if ($numrows>0){
+		if ($numrows > 0) {
 			
 			?>
 			<div class="table-responsive">
@@ -56,12 +55,12 @@
 					<th class='text-center' style="width: 36px;">Agregar</th>
 				</tr>
 				<?php
-				while ($row=mysqli_fetch_array($query)){
-					$id_producto=$row['id_producto'];
-					$codigo_producto=$row['codigo_producto'];
-					$nombre_producto=$row['nombre_producto'];
-					$precio_venta=$row["precio_producto"];
-					$precio_venta=number_format($precio_venta,2,'.','');
+				while ($row = mysqli_fetch_array($query)) {
+					$id_producto = $row['id_producto'];
+					$codigo_producto = $row['codigo_producto'];
+					$nombre_producto = $row['nombre_producto'];
+					$precio_venta = $row["precio_producto"];
+					$precio_venta = number_format($precio_venta, 2, '.', '');
 					?>
 					<tr>
 						<td><?php echo $codigo_producto; ?></td>
@@ -73,7 +72,7 @@
 						<td class='col-xs-2'><div class="pull-right">
 						<input type="text" class="form-control" style="text-align:right" id="precio_venta_<?php echo $id_producto; ?>"  value="<?php echo $precio_venta;?>" >
 						</div></td>
-						<td class='text-center'><a class='btn btn-info'href="#" onclick="agregar('<?php echo $id_producto ?>')"><i class="glyphicon glyphicon-plus"></i></a></td>
+						<td class='text-center'><a class='btn btn-info' href="#" onclick="agregar('<?php echo $id_producto ?>')"><i class="glyphicon glyphicon-plus"></i></a></td>
 					</tr>
 					<?php
 				}
